d34a22c13413d2473c42d73ee5b4f4a2337bd9d0
MarcosVel <marcos.veloso@sempreceub.com>
2020-09-16 01:03:44 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -25,16 +25,15 @@ public class Game implements Serializable {
 	private Long id;
 	private String title;
 	private Platform platform;
-	
+		
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
 	private Genre genre;
-	
+		
 	@OneToMany(mappedBy = "game")
 	private List<Record> records = new ArrayList<>();
 	
-	public Game() {
-		
+	public Game() {		
 	}
 
 	public Game(Long id, String title, Platform platform, Genre genre) {

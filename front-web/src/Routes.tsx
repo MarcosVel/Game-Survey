8cc863427630e47c2d0b030d8e67f8e94ebfd6cc
MarcosVel <marcos.veloso@sempreceub.com>
2020-09-18 01:12:15 -0300

    Adicionando a página de gráficos

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Records from './pages/Records';
+import Charts from './pages/Charts';
 
 const Routes = () => (
     <BrowserRouter>
@@ -13,6 +14,9 @@ const Routes = () => (
             </Route>
             <Route path="/records">
                 <Records />
+            </Route>
+            <Route path="/charts">
+                <Charts />
             </Route>
         </Switch>
     </BrowserRouter>
---
+++ front-web/src/pages/Charts/types.ts
@@ -0,0 +1,8 @@
+import { type } from "os";
+import { Platform } from "../Records/types";
+
+export type Game = {
+    id: number,
+    title: string,
+    platform: Platform;
+}

a7631cc55fa85c03a068ee70a77db8d2a2b5325e
MarcosVel <marcos.veloso@sempreceub.com>
2020-09-18 22:26:05 -0300

    Configurando front-web para produção

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters';
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-marcosveloso.herokuapp.com';
 
 const Records = () => {
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
